7813fa7a279e316a80e5d102aaf685bb4f3e7304
Ashley Kapaso <ashleykapaso@gmail.com>
2024-02-26 19:46:34 +0200

    remove setup files

--- OpenGL/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/prokingk/Projects/School/COS344/OpenGL
-# Build directory: /home/prokingk/Projects/School/COS344/OpenGL/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
