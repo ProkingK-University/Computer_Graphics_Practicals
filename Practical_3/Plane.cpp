1c95a6ea0067b232262da82a6f119ceae1191c90
Ashley Kapaso <ashleykapaso@gmail.com>
2024-04-21 09:03:06 +0200

    add plane

--- Practical_3/Plane.cpp
+++ Practical_3/Plane.cpp
@@ -2,17 +2,13 @@
 
 Plane::Plane()
 {
-    numShapes = 5;
+    numShapes = 6;
     shapes = new Shape *[numShapes];
 
-    // Body of the plane
-    shapes[0] = new Box(Vector(3, new double[3]{0, 0, 0}), 1.0, 0.1, 0.2, Vector(3, new double[3]{1, 0, 0}));
-
-    // Wings of the plane
-    shapes[1] = new Box(Vector(3, new double[3]{0, -0.5, 0}), 0.6, 0.02, 0.2, Vector(3, new double[3]{0, 1, 0}));
-    shapes[2] = new Box(Vector(3, new double[3]{0, 0.5, 0}), 0.6, 0.02, 0.2, Vector(3, new double[3]{0, 1, 0}));
-
-    // Tail of the plane
-    shapes[3] = new Box(Vector(3, new double[3]{-0.5, 0, 0}), 0.2, 0.1, 0.05, Vector(3, new double[3]{1, 0, 0}));
-    shapes[4] = new Box(Vector(3, new double[3]{-0.5, 0, 0.125}), 0.1, 0.02, 0.1, Vector(3, new double[3]{0, 0, 1}));
+    shapes[0] = new Box(Vector(3, new double[3]{0.5, 0.5, 0.5}), 0.1, 0.1, 0.3, Vector(3, new double[3]{1, 0, 0}));
+    shapes[1] = new TriangularPrism(Vector(3, new double[3]{0.35, 0.5, 0.5}), 0.01, 0.3, 0.1, Vector(3, new double[3]{0, 1, 0}));
+    shapes[2] = new TriangularPrism(Vector(3, new double[3]{0.65, 0.5, 0.5}), 0.01, 0.3, 0.1, Vector(3, new double[3]{0, 1, 0}));
+    shapes[3] = new Cone(20, Vector(3, new double[3]{0.5, 0.5, 0.35}), 0.05, 0.1, Vector(3, new double[3]{0, 0, 1}));
+    shapes[4] = new Cylinder(20, Vector(3, new double[3]{0.45, 0.45, 0.6}), 0.02, 0.1, Vector(3, new double[3]{1, 1, 0}));
+    shapes[5] = new Cylinder(20, Vector(3, new double[3]{0.55, 0.45, 0.6}), 0.02, 0.1, Vector(3, new double[3]{1, 1, 0}));
 }
--- Practical_3/Plane.h
+++ Practical_3/Plane.h
@@ -2,6 +2,9 @@
 #define PLANE_H
 
 #include "Box.h"
+#include "Cone.h"
+#include "Cylinder.h"
+#include "TriangularPrism.h"
 
 class Plane : public Shape
 {
