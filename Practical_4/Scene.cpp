0665476c05f9f934e409198ad69444af0475a3e9
Ashley Kapaso <ashleykapaso@gmail.com>
2024-07-27 07:20:49 +0200

    update scene

--- Practical_4/Scene.cpp
+++ Practical_4/Scene.cpp
@@ -9,10 +9,10 @@ Scene::Scene()
     shapes[0] = new Box(Vector(3, new double[3]{0, -0.1, 0}), 0.01, 0.8, 0.8, Vector(3, new double[3]{0.5, 0.5, 0.5}));
 
     // Wall
-    shapes[1] = new Box(Vector(3, new double[3]{0, 0, -0.1}), 0.6, 0.8, 0.05, Vector(3, new double[3]{1, 0, 0}));
+    shapes[1] = new Box(Vector(3, new double[3]{0, 0, -0.1}), 0.6, 0.8, 0.02, Vector(3, new double[3]{1, 0, 0}));
 
     // Window
-    shapes[2] = new Box(Vector(3, new double[3]{0, 0, 0}), 0.6, 0.8, 0.1, Vector(3, new double[3]{0, 0.8, 0}));
+    shapes[2] = new Box(Vector(3, new double[3]{0, 0, 0}), 0.6, 0.8, 0.05, Vector(3, new double[3]{0, 0.8, 0}));
 
     // Sphere
     shapes[3] = new Sphere(20, Vector(3, new double[3]{0.0, 0.0, 0.4}), 0.1, Vector(3, new double[3]{0, 0, 1}));
